fb8a49b6998d1880308d2ed8f0bf50e2ea882189
Borko Stankovic <54279922+borkostankovic83@users.noreply.github.com>
2020-01-16 15:33:26 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Apartements for rent">
+	<meta name="keywords" content="room,apartment,Tivat,Montenegro,aparmani,villa">
+	<meta name="author" content="Borko Stankovic">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+
+    <link href='fonts/fontawesome/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+    <link href='fonts/icomoon/style.css' rel='stylesheet' type='text/css'>
+
+    <link href='css/jquery-ui.min.css' rel='stylesheet' type='text/css'>
+    <link href='bootstrap/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='css/animate.css' rel='stylesheet' type='text/css'>
+    <link href='css/swiper.min.css' rel='stylesheet' type='text/css'>
+
+    <link href='css/style.css' rel='stylesheet' type='text/css'>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <title>Villa Stankovic</title>
+
+</head>
+<body class="loading">
+    <h1 class="hidden">Contact us</h1>
+    <div id="preloader-wrapper">
+        <div id="preloader"></div>
+    </div>
+    <header >
+        <div class="container">
+            <a href="index.html" class="logo-link"><img class="logo" src="images/logo.png" alt="Logo"></a>
+            <nav class="main-menu clearfix">
+                <h2 class="hidden">Main Menu</h2>
+                <ul>
+                    <li class="menu-item">
+                        <a href="index.html">Home</a>                      
+                    </li>
+                    <li class="menu-item">
+                        <a href="about.html">About</a>
+                    <li class="menu-item">
+                        <a href="facilities.html">Services</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="gallery.html">Gallery</a>
+                        <ul class="submenu">
+                            <li class="sub-menu-item"><a href="gallery-single1.html">Apartment With Terrace</a></li>
+							<li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
+							<li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
+							<li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item">
+                        <a href="blog1.html">Blog</a>                    
+                    </li>
+                    <li class="menu-item">
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div id="menu-toggle">
+                <div class="bar first"></div>
+                <div class="bar second"></div>
+                <div class="bar third"></div>
+            </div>
+        </div>
+
+        <nav id="mobile-menu">
+            <h2 class="hidden">Mobile Navigation Menu</h2>
+            <ul>
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="index.html">Home</a>
+                </li>
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="about.html">About</a>
+                </li>
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="facilities.html">Services</a>
+                </li>				
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="#">Gallery<i class="toggle-state-icon icon-angle-right"></i></a>
+                    <ul class="submenu">
+                            <li class="sub-menu-item"><a href="gallery-single1.html">Apartment With Terrace</a></li>
+							<li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
+							<li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
+							<li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                    </ul>
+                </li>
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="blog1.html">Blog<i class="toggle-state-icon icon-angle-right"></i></a>
+                </li>
+                <li class="mobile-menu-item">
+                    <a class="menu-item" href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="section-breadcrumb">
+        <h2 class="title" >About Villa Stankovic</h2>
+        <div class="breadcrumb">
+            You are here: <span class="slug"><span class="home"> Home </span> <span class="page"> > Contact Us</span></span>
+        </div>
+    </section>
+
+    <section class="section-style-2 section-contact-us">
+        <h2 class="hidden">Contact us</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="map-wrapper">
+                        <div class="map">
+                            
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2944.381448417276!2d18.687405815072022!3d42.44089943798584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134c3172c55d8a9f%3A0x1a0a007bfe0f841b!2sVilla+Stankovic!5e0!3m2!1sen!2s!4v1487742711297" 
+							width="600" height="420" frameborder="0" style="border:0" allowfullscreen>
+							</iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h3 class="title" >Villa Stankovic</h3>
+                    <div class="section-starter"></div>
+                    <p class="content">Villa is located close to Jadranska magistrala near fire department. The fresh scent of sea air and very close to the sea make this Villa ideal for rest.</p>
+                    <div class="row">
+                        <div class="col-sm-6 office-box" >
+                            <h4 class="subtitle">Headquarter</h4>
+                            <div class="address-line"><i class="fa fa-map-marker"></i>22 Brace Stipanic, Tivat, Montenegro</div>
+                            <div class="address-line"><i class="fa fa-phone"></i>+382(67)557-770 Viber</div>
+                            <div class="address-line"><i class="fa fa-envelope"></i>momirkas@gmail.com</div>
+                            <div class="address-line"><i class="fa fa-globe"></i>http://www.villa-stankovic.com</div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+			
+			<?php
+				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+					
+					require ('../mysqli_connect2.php');//connect to db
+
+					$errors = array(); // Initialize an error array.
+					
+					// Check for a last name:
+					if (empty($_POST['userName'])) {
+						echo 'required="required"/> ';
+					} else {
+						$userName = mysqli_real_escape_string($dbc, trim($_POST['userName']));
+					}
+
+					// Check for an email address:
+					if (empty($_POST['userEmail'])) {
+						echo 'required="required"/> ';
+					} else {
+						$userEmail= mysqli_real_escape_string($dbc, trim($_POST['userEmail']));
+					}
+					// Check for a mobilni:
+					if (empty($_POST['userPhone'])) {
+						echo '';
+					} else {
+						$userPhone = mysqli_real_escape_string($dbc, trim($_POST['userPhone']));
+					}
+
+					// Check for an text:
+					if (empty($_POST['userMsg'])) {
+						echo 'required="required"/> ';
+					} else {
+						$userMsg= mysqli_real_escape_string($dbc, trim($_POST['userMsg']));
+					}
+
+					if (empty($errors)) { // If everything's OK.
+					
+					// Make the query:
+					 $q = "INSERT INTO musterije (ime, email, mobilni, pitanja) VALUES
+					('$userName', '$userEmail','$userPhone', '$userMsg' )";
+							
+					$r = @mysqli_query ($dbc, $q); // Run the query.
+
+					 if ($r) { // If it ran OK.
+
+						 // Assign to message
+						 $body = "Ime:  {$_POST['userName']}\n\nEmail: {$_POST['userEmail']}\n\nMobilni: {$_POST['userPhone']}\n\nPitanje: {$_POST['userMsg']}";
+						
+						// make it no longer than 70 characters long:
+						$body = wordwrap($body, 70);
+						
+								$recipients = array('momirkas@gmail.com', 'borkostankovic83@yahoo.com');
+
+						// Send the mail:)
+						mail(implode(',', $recipients), 'Privatni Smestaj 2019', $body, "Od: {$_POST['userEmail']}");
+							
+						// Clear $_POST (so that the form's not sticky):
+						$_POST = array();
+
+								 // Print a message:
+						 echo '<h1>Thank you for getting in touch!
+						 <p>We appreciate you contacting us. We try to respond as soon as possible.Have a great day ahead!</p></h1><p>
+						<br /></p>';
+
+					 } else { // If it did not run OK.
+
+					 // Public message:
+					 echo '<h1>System Error</h1>
+					 <p class="error">You could not insert data due to a system error. We apologize for
+					any inconvenience.</p>';
+
+					 // Debugging message:
+					 echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
+					 }// End of if ($r) IF.
+					 
+					 //exit();
+					
+					} else { // Report the errors.
+					
+						echo '<h1>Error!</h1>
+						<p class="error">The following error(s) occurred:<br />';
+						foreach ($errors as $msg) { // Print each error.
+							echo " - $msg<br />\n";
+						}
+						echo '</p><p>Please try again.</p><p><br /></p>';
+						
+					} // End of if (empty($errors)) IF.
+					
+					mysqli_close($dbc); // Close the database connection.
+					
+				} // End of the main Submit conditional.
+			?>
+			
+            <div class="row">
+                <div class="col-md-6">
+                    <h3 class="title" >Drop us a line</h3>
+                    <div class="section-starter"></div>
+                    <form method="post" action="contact.php" class="contact-form">
+                        <div class="form-group">
+                            <span><label class="control-label" >Name *</label></span>
+                            <span><input name="userName" type="text" class="form-control" placeholder="Full Name"
+                required="required"></span>
+                        </div>
+                        <div class="form-group">
+                            <span><label class="control-label">Email *</label></span>
+                            <span><input name="userEmail" type="text" class="form-control" placeholder="Email"
+                required="required" ></span>
+                        </div>
+						<div class="form-group">
+                            <span><label class="control-label">Mobile *</label></span>
+                            <span><input name="userPhone" type="text" class="form-control" placeholder="Mobile number"
+                required="required" ></span>
+                        </div>
+                        <div class="form-group">
+                            <span><label class="control-label">Message *</label></span>
+                            <span><textarea class="form-control" name="userMsg" placeholder="Ask us question."
+                required="required"></textarea></span>
+                        </div>
+    <div class="g-recaptcha" data-sitekey="6Lc_GogUAAAAAO0Pr-s6_uTJr6YhtGaPu4oCNx07" required="required"></div><hr>
+
+                        <span><button class="button secondary transparent">Send Us</button></span>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/contact-bg.jpg" class="img-centered img-responsive" alt="contact-bg">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h3 class="subtitle"><strong>Useful Links</strong></h3>
+                    <ul class="site-links">
+                        <li><a target="_blank" href="http://opstinativat.com/index.php?lang=en">Tivat Municipality</a></li>
+                        <li><a target="_blank" href="http://www.portomontenegro.com/">Porto Montenegro</a></li>
+                        <li><a target="_blank" href="http://www.montenegroinside.com/en/sights/">Attractions</a></li>
+                        <li><a target="_blank" href="www.montenegroairports.com/eng/">Airport Tivat</a></li>
+                        <li><a target="_blank" href="https://busticket4.me/bus-station/departures/Tivat/EN/204">Bus Transportation</a></li>
+                        <li><a target="_blank" href="http://www.rentalcars.com/en/city/me/tivat/?affiliateCode=google&preflang=en&label=tivat-5m6Y_**26NHifDXk5jPTSwS80496127849&ws=&gclid=CPKSy6qtrNECFUtsGwodniYH4Q">Car Rentals</a></li>
+                        <li><a target="_blank" href="http://www.redtaxi.me/">Red Taxi</a></li>
+                        <li><a target="_blank" href="http://radiotivat.com/">Radio Tivat</a></li>
+                        <li><a target="_blank" href="#">Under Construction</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <h3 class="subtitle wide">Villa <strong>Stankovic</strong></h3>
+                    <div class="moon-divider small"></div>
+                    <p>Brace Stipanic 22, Tivat, Montenegro</p>
+                    <p>(+382) - 67 - 557770   —   Viber</p>
+                    <p><a href="#">momirkas@gmail.com</a></p>
+                    <p><a href="#">http://www.villa-stankovic.com</a></p>
+                    <div class="moon-divider small"></div>
+                    <div class="social-links">
+                        <a target="_blank" class="social-link" href="https://www.facebook.com/vila.stankovic?fref=ts"><i class="fa fa-facebook"></i><i class="fa fa-facebook"></i></a>
+                        <a target="_blank" class="social-link" href="https://twitter.com/villa_stankovic"><i class="fa fa-twitter"></i><i class="fa fa-twitter"></i></a>
+                        <a target="_blank" class="social-link" href="https://www.youtube.com/watch?v=_LjIFk-ZvyU&t=21s"><i class="fa fa-youtube"></i><i class="fa fa-youtube"></i></a>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-right">
+                    <h3 class="subtitle"><strong>About Tivat</strong></h3>
+                    <p>Tivat is situated in the central part of Boka Kotorska.
+					From the north it is protected by the Mountain Lovcen (1749 m) and Lovcen`s spur Vrmac (710 m).
+					Tivat bay through gorge Verige is connected with Risan and Kotor bay.
+					Tivat, whose total surface is 49 km2 is considered to be the most wide spread lodgment in Boka Kotorska.</p>                   
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <p>@2015 Borko Stankovic. Designed with <i class="fa fa-heart"></i></p>
+            </div>
+        </div>
+    </footer>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="js/functions.js"></script>
+</body>
+</html>
