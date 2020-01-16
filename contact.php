7aa2bf76a2c367c337380d6a202e5896bcba681f
Borko Stankovic <54279922+borkostankovic83@users.noreply.github.com>
2020-01-16 16:01:45 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
+
 <head>
 
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta name="description" content="Apartements for rent">
-	<meta name="keywords" content="room,apartment,Tivat,Montenegro,aparmani,villa">
-	<meta name="author" content="Borko Stankovic">
+    <meta name="keywords" content="room,apartment,Tivat,Montenegro,aparmani,villa">
+    <meta name="author" content="Borko Stankovic">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
     <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-
     <link href='fonts/fontawesome/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
     <link href='fonts/icomoon/style.css' rel='stylesheet' type='text/css'>
-
     <link href='css/jquery-ui.min.css' rel='stylesheet' type='text/css'>
     <link href='bootstrap/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='css/animate.css' rel='stylesheet' type='text/css'>
     <link href='css/swiper.min.css' rel='stylesheet' type='text/css'>
-
     <link href='css/style.css' rel='stylesheet' type='text/css'>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
     <title>Villa Stankovic</title>
 
 </head>
+
 <body class="loading">
     <h1 class="hidden">Contact us</h1>
     <div id="preloader-wrapper">
         <div id="preloader"></div>
     </div>
-    <header >
+    <header>
         <div class="container">
             <a href="index.html" class="logo-link"><img class="logo" src="images/logo.png" alt="Logo"></a>
             <nav class="main-menu clearfix">
                 <h2 class="hidden">Main Menu</h2>
                 <ul>
                     <li class="menu-item">
-                        <a href="index.html">Home</a>                      
+                        <a href="index.html">Home</a>
                     </li>
                     <li class="menu-item">
                         <a href="about.html">About</a>
@@ -48,13 +45,14 @@
                         <a href="gallery.html">Gallery</a>
                         <ul class="submenu">
                             <li class="sub-menu-item"><a href="gallery-single1.html">Apartment With Terrace</a></li>
-							<li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
-							<li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
-							<li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                            <li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
+                            <li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
+                            <li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                            <li class="sub-menu-item"><a href="gallery-single6.html">A Penthouse Apartment</a></li>
                         </ul>
                     </li>
                     <li class="menu-item">
-                        <a href="blog1.html">Blog</a>                    
+                        <a href="blog1.html">Blog</a>
                     </li>
                     <li class="menu-item">
                         <a href="contact.php">Contact</a>
@@ -80,14 +78,15 @@
                 </li>
                 <li class="mobile-menu-item">
                     <a class="menu-item" href="facilities.html">Services</a>
-                </li>				
+                </li>
                 <li class="mobile-menu-item">
                     <a class="menu-item" href="#">Gallery<i class="toggle-state-icon icon-angle-right"></i></a>
                     <ul class="submenu">
-                            <li class="sub-menu-item"><a href="gallery-single1.html">Apartment With Terrace</a></li>
-							<li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
-							<li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
-							<li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                        <li class="sub-menu-item"><a href="gallery-single1.html">Apartment With Terrace</a></li>
+                        <li class="sub-menu-item"><a href="gallery-single2.html">Apartment With Garden</a></li>
+                        <li class="sub-menu-item"><a href="gallery-single3.html">Studio Apartment</a></li>
+                        <li class="sub-menu-item"><a href="gallery-single4.html">3b Condo</a></li>
+                        <li class="sub-menu-item"><a href="gallery-single6.html">A Penthouse Apartment</a></li>
                     </ul>
                 </li>
                 <li class="mobile-menu-item">
@@ -101,7 +100,7 @@
     </header>
 
     <section class="section-breadcrumb">
-        <h2 class="title" >About Villa Stankovic</h2>
+        <h2 class="title">About Villa Stankovic</h2>
         <div class="breadcrumb">
             You are here: <span class="slug"><span class="home"> Home </span> <span class="page"> > Contact Us</span></span>
         </div>
@@ -114,150 +113,146 @@
                 <div class="col-md-6">
                     <div class="map-wrapper">
                         <div class="map">
-                            
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2944.381448417276!2d18.687405815072022!3d42.44089943798584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134c3172c55d8a9f%3A0x1a0a007bfe0f841b!2sVilla+Stankovic!5e0!3m2!1sen!2s!4v1487742711297" 
-							width="600" height="420" frameborder="0" style="border:0" allowfullscreen>
-							</iframe>
+
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2944.381448417276!2d18.687405815072022!3d42.44089943798584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134c3172c55d8a9f%3A0x1a0a007bfe0f841b!2sVilla+Stankovic!5e0!3m2!1sen!2s!4v1487742711297" width="600" height="420" frameborder="0" style="border:0" allowfullscreen>
+                            </iframe>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <h3 class="title" >Villa Stankovic</h3>
+                    <h3 class="title">Villa Stankovic</h3>
                     <div class="section-starter"></div>
                     <p class="content">Villa is located close to Jadranska magistrala near fire department. The fresh scent of sea air and very close to the sea make this Villa ideal for rest.</p>
                     <div class="row">
-                        <div class="col-sm-6 office-box" >
+                        <div class="col-sm-6 office-box">
                             <h4 class="subtitle">Headquarter</h4>
                             <div class="address-line"><i class="fa fa-map-marker"></i>22 Brace Stipanic, Tivat, Montenegro</div>
                             <div class="address-line"><i class="fa fa-phone"></i>+382(67)557-770 Viber</div>
                             <div class="address-line"><i class="fa fa-envelope"></i>momirkas@gmail.com</div>
                             <div class="address-line"><i class="fa fa-globe"></i>http://www.villa-stankovic.com</div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
-			
-			<?php
-				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-					
-					require ('../mysqli_connect2.php');//connect to db
 
-					$errors = array(); // Initialize an error array.
-					
-					// Check for a last name:
-					if (empty($_POST['userName'])) {
-						echo 'required="required"/> ';
-					} else {
-						$userName = mysqli_real_escape_string($dbc, trim($_POST['userName']));
-					}
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-					// Check for an email address:
-					if (empty($_POST['userEmail'])) {
-						echo 'required="required"/> ';
-					} else {
-						$userEmail= mysqli_real_escape_string($dbc, trim($_POST['userEmail']));
-					}
-					// Check for a mobilni:
-					if (empty($_POST['userPhone'])) {
-						echo '';
-					} else {
-						$userPhone = mysqli_real_escape_string($dbc, trim($_POST['userPhone']));
-					}
+                require('../mysqli_connect2.php'); //connect to db
 
-					// Check for an text:
-					if (empty($_POST['userMsg'])) {
-						echo 'required="required"/> ';
-					} else {
-						$userMsg= mysqli_real_escape_string($dbc, trim($_POST['userMsg']));
-					}
+                $errors = array(); // Initialize an error array.
 
-					if (empty($errors)) { // If everything's OK.
-					
-					// Make the query:
-					 $q = "INSERT INTO musterije (ime, email, mobilni, pitanja) VALUES
+                // Check for a last name:
+                if (empty($_POST['userName'])) {
+                    echo 'required="required"/> ';
+                } else {
+                    $userName = mysqli_real_escape_string($dbc, trim($_POST['userName']));
+                }
+
+                // Check for an email address:
+                if (empty($_POST['userEmail'])) {
+                    echo 'required="required"/> ';
+                } else {
+                    $userEmail = mysqli_real_escape_string($dbc, trim($_POST['userEmail']));
+                }
+                // Check for a mobilni:
+                if (empty($_POST['userPhone'])) {
+                    echo '';
+                } else {
+                    $userPhone = mysqli_real_escape_string($dbc, trim($_POST['userPhone']));
+                }
+
+                // Check for an text:
+                if (empty($_POST['userMsg'])) {
+                    echo 'required="required"/> ';
+                } else {
+                    $userMsg = mysqli_real_escape_string($dbc, trim($_POST['userMsg']));
+                }
+
+                if (empty($errors)) { // If everything's OK.
+
+
+                    // Make the query:
+                    $q = "INSERT INTO musterije (ime, email, mobilni, pitanja) VALUES
 					('$userName', '$userEmail','$userPhone', '$userMsg' )";
-							
-					$r = @mysqli_query ($dbc, $q); // Run the query.
 
-					 if ($r) { // If it ran OK.
+                    $r = @mysqli_query($dbc, $q); // Run the query.
 
-						 // Assign to message
-						 $body = "Ime:  {$_POST['userName']}\n\nEmail: {$_POST['userEmail']}\n\nMobilni: {$_POST['userPhone']}\n\nPitanje: {$_POST['userMsg']}";
-						
-						// make it no longer than 70 characters long:
-						$body = wordwrap($body, 70);
-						
-								$recipients = array('momirkas@gmail.com', 'borkostankovic83@yahoo.com');
+                    if ($r) { // If it ran OK.
 
-						// Send the mail:)
-						mail(implode(',', $recipients), 'Privatni Smestaj 2019', $body, "Od: {$_POST['userEmail']}");
-							
-						// Clear $_POST (so that the form's not sticky):
-						$_POST = array();
+                        // Assign to message
+                        $body = "Ime:  {$_POST['userName']}\n\nEmail: {$_POST['userEmail']}\n\nMobilni: {$_POST['userPhone']}\n\nPitanje: {$_POST['userMsg']}";
 
-								 // Print a message:
-						 echo '<h1>Thank you for getting in touch!
+                        // make it no longer than 70 characters long:
+                        $body = wordwrap($body, 70);
+
+                        $recipients = array('momirkas@gmail.com', 'borkostankovic83@yahoo.com');
+
+
+                        // Send the mail:)
+                        mail(implode(',', $recipients), 'Privatni Smestaj 2020', $body, "Od: {$_POST['userEmail']}");
+
+                        // Clear $_POST (so that the form's not sticky):
+                        $_POST = array();
+
+                        // Print a message:
+                        echo '<h1>Thank you for getting in touch!
 						 <p>We appreciate you contacting us. We try to respond as soon as possible.Have a great day ahead!</p></h1><p>
 						<br /></p>';
+                    } else { // If it did not run OK.
 
-					 } else { // If it did not run OK.
-
-					 // Public message:
-					 echo '<h1>System Error</h1>
+                        // Public message:
+                        echo '<h1>System Error</h1>
 					 <p class="error">You could not insert data due to a system error. We apologize for
 					any inconvenience.</p>';
 
-					 // Debugging message:
-					 echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-					 }// End of if ($r) IF.
-					 
-					 //exit();
-					
-					} else { // Report the errors.
-					
-						echo '<h1>Error!</h1>
+                        // Debugging message:
+                        echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
+                    } // End of if ($r) IF.
+
+                    //exit();
+
+                } else { // Report the errors.
+
+                    echo '<h1>Error!</h1>
 						<p class="error">The following error(s) occurred:<br />';
-						foreach ($errors as $msg) { // Print each error.
-							echo " - $msg<br />\n";
-						}
-						echo '</p><p>Please try again.</p><p><br /></p>';
-						
-					} // End of if (empty($errors)) IF.
-					
-					mysqli_close($dbc); // Close the database connection.
-					
-				} // End of the main Submit conditional.
-			?>
-			
+                    foreach ($errors as $msg) { // Print each error.
+                        echo " - $msg<br />\n";
+                    }
+                    echo '</p><p>Please try again.</p><p><br /></p>';
+                } // End of if (empty($errors)) IF.
+
+                mysqli_close($dbc); // Close the database connection.
+
+            } // End of the main Submit conditional.
+            ?>
+
             <div class="row">
                 <div class="col-md-6">
-                    <h3 class="title" >Drop us a line</h3>
+                    <h3 class="title">Drop us a line</h3>
                     <div class="section-starter"></div>
-                    <form method="post" action="contact.php" class="contact-form">
+                    <form id='demo-form' method="post" action="contact.php" class="contact-form">
                         <div class="form-group">
-                            <span><label class="control-label" >Name *</label></span>
-                            <span><input name="userName" type="text" class="form-control" placeholder="Full Name"
-                required="required"></span>
+                            <span><label class="control-label">Name *</label></span>
+                            <span><input name="userName" type="text" class="form-control" placeholder="Full Name" required="required"></span>
                         </div>
                         <div class="form-group">
                             <span><label class="control-label">Email *</label></span>
-                            <span><input name="userEmail" type="text" class="form-control" placeholder="Email"
-                required="required" ></span>
+                            <span><input name="userEmail" type="email" class="form-control" placeholder="Email" required="required"></span>
                         </div>
-						<div class="form-group">
-                            <span><label class="control-label">Mobile *</label></span>
-                            <span><input name="userPhone" type="text" class="form-control" placeholder="Mobile number"
-                required="required" ></span>
+                        <div class="form-group">
+                            <span><label class="control-label">Mobile</label></span>
+                            <span><input name="userPhone" type="tel" pattern="\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|
+                            2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$" class="form-control" placeholder="+3024567892"></span>
                         </div>
                         <div class="form-group">
                             <span><label class="control-label">Message *</label></span>
-                            <span><textarea class="form-control" name="userMsg" placeholder="Ask us question."
-                required="required"></textarea></span>
+                            <span><textarea class="form-control" name="userMsg" placeholder="Ask us question." required="required"></textarea></span>
                         </div>
-    <div class="g-recaptcha" data-sitekey="6Lc_GogUAAAAAO0Pr-s6_uTJr6YhtGaPu4oCNx07" required="required"></div><hr>
+                        <!-- <div class="g-recaptcha" data-sitekey="6LdcC8wUAAAAAG2HJaw19ZViaeDzuzFAPUk4IAP1" required="required"></div><hr> -->
 
-                        <span><button class="button secondary transparent">Send Us</button></span>
+                        <span><button class=" button secondary transparent" data-sitekey="6LdcC8wUAAAAAG2HJaw19ZViaeDzuzFAPUk4IAP1">Send Us</button></span>
                     </form>
                 </div>
                 <div class="col-md-6">
@@ -288,7 +283,7 @@
                     <h3 class="subtitle wide">Villa <strong>Stankovic</strong></h3>
                     <div class="moon-divider small"></div>
                     <p>Brace Stipanic 22, Tivat, Montenegro</p>
-                    <p>(+382) - 67 - 557770   —   Viber</p>
+                    <p>(+382) - 67 - 557770 — Viber</p>
                     <p><a href="#">momirkas@gmail.com</a></p>
                     <p><a href="#">http://www.villa-stankovic.com</a></p>
                     <div class="moon-divider small"></div>
@@ -301,9 +296,9 @@
                 <div class="col-sm-4 text-right">
                     <h3 class="subtitle"><strong>About Tivat</strong></h3>
                     <p>Tivat is situated in the central part of Boka Kotorska.
-					From the north it is protected by the Mountain Lovcen (1749 m) and Lovcen`s spur Vrmac (710 m).
-					Tivat bay through gorge Verige is connected with Risan and Kotor bay.
-					Tivat, whose total surface is 49 km2 is considered to be the most wide spread lodgment in Boka Kotorska.</p>                   
+                        From the north it is protected by the Mountain Lovcen (1749 m) and Lovcen`s spur Vrmac (710 m).
+                        Tivat bay through gorge Verige is connected with Risan and Kotor bay.
+                        Tivat, whose total surface is 49 km2 is considered to be the most wide spread lodgment in Boka Kotorska.</p>
                 </div>
             </div>
         </div>
@@ -320,4 +315,5 @@
     <script type="text/javascript" src="js/plugins.js"></script>
     <script type="text/javascript" src="js/functions.js"></script>
 </body>
+
 </html>
